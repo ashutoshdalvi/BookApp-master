fa137601bc28d235f0e51fe40c8163893f967319
ashutoshdalvi <ashutoshdalvi29@gmail.com>
2018-10-03 12:33:53 +0530

    With Firebase

--- app/src/main/java/com/example/ashutosh_dalvi/bookapp/Login.java
+++ app/src/main/java/com/example/ashutosh_dalvi/bookapp/Login.java
@@ -3,23 +3,37 @@ package com.example.ashutosh_dalvi.bookapp;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.PrivateKey;
 
 public class Login extends AppCompatActivity {
-    private TextView txt;
+    private EditText etmail,etpassword;
+    private String email,password;
+    private FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        etmail = (EditText)findViewById(R.id.email);
+        etpassword= (EditText)findViewById(R.id.password);
+        auth = FirebaseAuth.getInstance();
 
     }
 
@@ -29,7 +43,24 @@ public class Login extends AppCompatActivity {
 
     }
     public void cardclick(View view){
-        Intent i = new Intent(this,MainActivity.class);
-        startActivity(i);
+       try {
+            email=etmail.getText().toString();
+            password=etpassword.getText().toString();
+            auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent i = new Intent(Login.this, Homepage.class);
+                                startActivity(i);
+                                finish();
+                            }else{
+                                Toast.makeText(Login.this, "Invalid data", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }catch(Exception e){
+            Toast.makeText(Login.this, "Please enter valid data", Toast.LENGTH_SHORT).show();
+        }
     }
-    }
+}
--- app/src/main/java/com/example/ashutosh_dalvi/bookapp/Registration.java
+++ app/src/main/java/com/example/ashutosh_dalvi/bookapp/Registration.java
@@ -1,71 +1,88 @@
 package com.example.ashutosh_dalvi.bookapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/*import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;*/
+
 
 public class Registration extends AppCompatActivity {
 
-    //private DatabaseReference fdatabase;
-    private EditText name,mail_id,password,cpassword,contact;
-    private Button button;
-    private User user= new User();
-    //FirebaseDatabase ref;
+    private DatabaseReference fdatabase;
+    private EditText etname, etmail_id, etpassword, etcpassword, etcontact;
+    private CardView button;
+    private String name,email, password,contact,cpassword;
+    private User user = new User();
+    FirebaseDatabase ref;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        name = (EditText)findViewById(R.id.editText1);
-        mail_id = (EditText)findViewById(R.id.editText2);
-        contact = (EditText)findViewById(R.id.editText3);
-        password = (EditText)findViewById(R.id.editText4);
-        cpassword = (EditText)findViewById(R.id.editText5);
+        etname = (EditText) findViewById(R.id.editText1);
+        etmail_id = (EditText) findViewById(R.id.editText2);
+        etcontact = (EditText) findViewById(R.id.editText3);
+        etpassword = (EditText) findViewById(R.id.editText4);
+        etcpassword = (EditText) findViewById(R.id.editText5);
+        button = (CardView) findViewById(R.id.button);
 
-
-        /*button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Registration.this,MainActivity.class);
-                startActivity(i);
-
+                try {
+                    getdata();
+                    if (password.equals(cpassword)) {
+                        auth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            fdatabase.push().setValue(user);
+                                            Intent i = new Intent(Registration.this, MainActivity.class);
+                                            startActivity(i);
+                                            finish();
+                                        } else {
+                                            Toast.makeText(Registration.this, "Account already exists", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(Registration.this, "Password not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(Registration.this, "Please enter valid data", Toast.LENGTH_SHORT).show();
+                }
             }
-        });*/
+        });
 
-        //ref = FirebaseDatabase.getInstance();
-        //fdatabase = ref.getReference();
+        auth = FirebaseAuth.getInstance();
+        ref = FirebaseDatabase.getInstance();
+        fdatabase = ref.getReference("Users");
 
     }
-    public void getdata(){
-        user.setName(name.getText().toString());
-        user.setMail_id(mail_id.getText().toString());
-        user.setPassword(password.getText().toString());
-        user.setContact(contact.getText().toString());
+
+    public void getdata() {
+        email = etmail_id.getText().toString();
+        password = etpassword.getText().toString();
+        name = etname.getText().toString();
+        contact = etcontact.getText().toString();
+        cpassword= etcpassword.getText().toString();
+        user.setName(name);
+        user.setMail_id(email);
+        user.setPassword(password);
+        user.setContact(contact);
     }
-    /*public void signinconn(View view){
-        //fdatabase.addValueEventListener(new ValueEventListener() {
-        //    @Override
-         //   public void onDataChange(DataSnapshot dataSnapshot) {
-                getdata();
-                //fdatabase.push().setValue(user);
-               Toast.makeText(Registration.this, "Data Inserted...", Toast.LENGTH_SHORT).show();
-
-            }*/
-
-           // @Override
-          //  public void onCancelled(DatabaseError databaseError) {
-
-           // }
-        //});
-    }
-
-//}
+}
