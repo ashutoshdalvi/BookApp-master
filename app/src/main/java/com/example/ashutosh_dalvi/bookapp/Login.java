b64eeedbbfe0be1fd093beefff33f43b2dcadce4
MansiC <mansi.chougule09@gmail,com>
2018-10-13 22:38:55 +0530

    new flash screen

--- app/src/main/java/com/example/ashutosh_dalvi/bookapp/Login.java
+++ app/src/main/java/com/example/ashutosh_dalvi/bookapp/Login.java
@@ -39,6 +39,8 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
